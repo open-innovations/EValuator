a21a582bddddbde5523f0d8e0e92948ee8e37804
slowe <gravatar@strudel.org.uk>
2022-03-02 13:32:05 +0000

    Add MSOA names
    Names from the House of Commons Library

--- makeLookup.pl
+++ makeLookup.pl
@@ -3,6 +3,7 @@ use Data::Dumper;
 
 #https://www.arcgis.com/sharing/rest/content/items/7db6988a695f4c75989f0dc6701d4167/data <- zip file
 
+$namefile = "raw/MSOA-Names-Latest.csv";
 $lookupfile = "raw/PCD_OA_LSOA_MSOA_LAD_NOV21_UK_LU.csv";
 $lookupdir = "www/data/";
 
@@ -12,9 +13,32 @@ if(!-e $lookupfile){
 	exit;
 }
 
+if(!-e $namefile){
+	print "WARNING: No MSOA names file exists at $namefile\n";
+	print "You could download it from https://houseofcommonslibrary.github.io/msoanames/MSOA-Names-Latest.csv\n";
+	exit;
+}
+
 %msoa;
 %oa;
 %lad;
+
+open(FILE,$namefile);
+while(<FILE>){
+	#msoa11cd,msoa11nm,msoa11nmw,msoa11hclnm,msoa11hclnmw,Laname
+	#E02006534,Adur 001,Adur 001,Hillside,,Adur
+	$line = $_;
+	if($line){
+		$line =~ s/[\n\r]+//g;
+		($msoa11cd,$msoa11nm,$msoa11nmw,$msoa11hclnm,$msoa11hclnmw,$Laname) = split(/,/,$line);
+		if($msoa11cd !~ /msoa11cd/ && $msoa11hclnm){
+			$msoa11hclnm =~ s/(^\"|\"$)//g;
+			$msoa{$msoa11cd} = {'name'=>$msoa11hclnm};
+		}
+	}
+}
+close(FILE);
+
 open(FILE,$lookupfile);
 while(<FILE>){
 	#pcd7,pcd8,pcds,dointr,doterm,usertype,oa11cd,lsoa11cd,msoa11cd,ladcd,lsoa11nm,msoa11nm,ladnm,ladnmw
@@ -30,8 +54,11 @@ while(<FILE>){
 		$oa11cd =~ s/(^\"|\"$)//g;
 		# If it isn't the header we use it
 		if($pcd7 ne "pcd7"){
-			if($msoa11cd ne "" && !$msoa{$msoa11cd}){
-				$msoa{$msoa11cd} = $ladcd;
+			if($msoa11cd ne "" && $msoa11cd ne "msoa11cd"){
+				if(!$msoa{$msoa11cd}){
+					$msoa{$msoa11cd} = {'name'=>'?'};
+				}
+				$msoa{$msoa11cd}{'LAD'} = $ladcd;
 			}
 			if($oa11cd ne "" && !$oa{$oa11cd}){
 				$oa{$oa11cd} = {'lad'=>$ladcd,'lsoa'=>$lsoa11cd,'msoa'=>$msoa11cd};
@@ -48,10 +75,11 @@ while(<FILE>){
 }
 close(FILE);
 
+
 open(LOOKUP,">",$lookupdir."lookupMSOA.tsv");
 print LOOKUP "MSOA\tLAD\n";
 foreach $m (sort(keys(%msoa))){
-	print LOOKUP "$m\t$msoa{$m}\n"
+	print LOOKUP "$m\t$msoa{$m}{'LAD'}\n"
 }
 close(LOOKUP);
 
@@ -64,7 +92,7 @@ close(LOOKUP);
 
 
 open(LOOKUP,">",$lookupdir."lookupLAD.tsv");
-print LOOKUP "LAD\tLAD nAme\tMSOAs\n";
+print LOOKUP "LAD\tLAD name\tMSOAs\n";
 foreach $l (sort(keys(%lad))){
 	print LOOKUP "$l\t$lad{$l}{'name'}\t";
 	print LOOKUP join(";",sort(keys(%{$lad{$l}{'msoas'}})));
@@ -72,15 +100,6 @@ foreach $l (sort(keys(%lad))){
 }
 close(LOOKUP);
 
-
-open(LOOKUP,">",$lookupdir."lookupLAD.tsv");
-print LOOKUP "LAD\tMSOAs\n";
-foreach $l (sort(keys(%lad))){
-	print LOOKUP "$l\t";
-	print LOOKUP join(";",sort(keys(%{$lad{$l}{'msoas'}})));
-	print LOOKUP "\n";
-}
-close(LOOKUP);
 
 open(LOOKUP,">",$lookupdir."LAD.tsv");
 print LOOKUP "LAD\tLAD name\n";
@@ -93,7 +112,7 @@ foreach $l (sort(keys(%lad))){
 	open(LADLOOKUP,">",$lookupdir."LAD/$l/$l-msoas.tsv");
 	@msoas = sort(keys(%{$lad{$l}{'msoas'}}));
 	for($m = 0; $m < @msoas; $m++){
-		print LADLOOKUP "$msoas[$m]\n";
+		print LADLOOKUP "$msoas[$m]\t$msoa{$msoas[$m]}{'name'}\n";
 	}
 	close(LADLOOKUP);
 }
