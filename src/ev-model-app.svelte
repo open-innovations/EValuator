490a1b2b59f6d77f5da4ea71e8e2ae3cb9f9d4b6
Giles Dring <giles@dringtech.com>
2022-03-22 12:54:54 +0000

    Set area and msoa via search param

--- src/ev-model-app.svelte
+++ src/ev-model-app.svelte
@@ -6,25 +6,24 @@
   import Marker from './components/Marker.svelte';
   import ModelPane from './components/ModelPane.svelte';
 
+  import { location } from './stores/location';
+
   import { uk } from './lib/maps/bounds';
   import { greyscale } from './lib/maps/basemaps';
   import { lightCarto } from './lib/maps/labels';
 
   import ModelInputs from './components/model/ModelInputs.svelte';
   import EnergyModel from './components/model/EnergyModel.svelte';
-  import { getMapState, storeMapState } from './lib/location';
 
-  const { view, zoom } = getMapState();
-  let bounds = undefined;
-  if (!view) bounds = uk;
+  let bounds = uk;
 
   const models = [ EnergyModel ];
 
   let map;
 
-  const mapClick = e => location = e.latlng;
+  const mapClick = e => site = e.latlng;
 
-  let location = undefined;
+  let site = undefined;
   let modelParams = {
     slow: 0,
     fast: 0,
@@ -34,9 +33,10 @@
 
 
 <h1>EValuator - EV Bulk Charging Planner Model</h1>
+<p>{ $location?.msoa.properties.msoa11hclnm }</p>
 <section id='map' class="screen">
-  <Leaflet bind:map { bounds } { view } { zoom } baseLayer={ greyscale } labelLayer={ lightCarto } clickHandler={ mapClick }>
-    <Marker bind:latLng={ location }>
+  <Leaflet bind:map { bounds } baseLayer={ greyscale } labelLayer={ lightCarto } clickHandler={ mapClick }>
+    <Marker bind:latLng={ site }>
     </Marker>
   </Leaflet>
 </section>
