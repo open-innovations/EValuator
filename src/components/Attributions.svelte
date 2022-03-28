9840559c075964669b5ef6adfd5fb432c557b595
Giles Dring <giles@dringtech.com>
2022-03-28 15:37:27 +0100

    Move attributions to footer

--- src/components/Attributions.svelte
+++ src/components/Attributions.svelte
@@ -1,27 +1,15 @@
-<div>
-  <h2>
-    Attributions
-  </h2>
+<script>
+  function attach(node) {
+    const target = document.querySelector('#attribution');
+    node.querySelectorAll('li').forEach(li => target.appendChild(li));
+    node.remove();
+  }
+</script>
 
-  <p>
-    This prototype uses icons from <a href="https://thenounproject.com/">The Noun Project</a> under a CC license:
-
-    <a href="https://thenounproject.com/icon/energy-3321862/">Energy by Chintuza from NounProject.com</a>,
-    <!-- <a href="https://thenounproject.com/icon/pin-1069069/">Pin by Ralf Schmitzer from NounProject.com</a>, -->
-    <a href="https://thenounproject.com/icon/pin-1233347/">Pin by Drishya from NounProject.com</a>,
-    <a href="https://thenounproject.com/icon/co2-3844972/">Co2 by Soremba from NounProject.com</a>,
-    <a href="https://thenounproject.com/icon/warning-936898/">Warning by Thengakola from NounProject.com</a>
-  </p>
+<div hidden use:attach>
+  <li><a href="https://thenounproject.com/icon/energy-3321862/">Energy by Chintuza from NounProject.com</a></li>
+  <!-- <li><a href="https://thenounproject.com/icon/pin-1069069/">Pin by Ralf Schmitzer from NounProject.com</a></li> -->
+  <li><a href="https://thenounproject.com/icon/pin-1233347/">Pin by Drishya from NounProject.com</a></li>
+  <li><a href="https://thenounproject.com/icon/co2-3844972/">Co2 by Soremba from NounProject.com</a></li>
+  <li><a href="https://thenounproject.com/icon/warning-936898/">Warning by Thengakola from NounProject.com</a></li>
 </div>
-
-<style>
-  div {
-    font-size: 0.8em;
-    padding: 1rem;
-    background: #222;
-    color: #ddd;
-  }
-  a {
-    color: #ff7700;
-  }
-</style>
