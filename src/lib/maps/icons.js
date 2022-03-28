01cd5c087736a4f62fa63486b0282daac4f3b306
Giles Dring <giles@dringtech.com>
2022-03-28 10:04:30 +0100

    Changing icon for site

--- src/lib/maps/icons.js
+++ src/lib/maps/icons.js
@@ -1,9 +1,9 @@
 import L from '../vendor/leaflet';
 
 export const pin = L.icon({
-  iconUrl: 'resources/images/noun-pin-1069069.svg',
+  iconUrl: 'resources/images/noun-pin-1233347.svg',
   iconSize: 60,
-  iconAnchor: [32, 60],
+  iconAnchor: [32, 52],
   shadowUrl: 'resources/images/marker-shadow.png',
   shadowRetinaUrl: 'resources/images/marker-shadow.png',
   shadowSize: 60,
