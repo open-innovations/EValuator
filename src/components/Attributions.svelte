bc83e79cc244d2120f87bc0932f656ff27aa1193
Giles Dring <giles@dringtech.com>
2022-03-28 14:53:43 +0100

    Add warning triangle to warning

--- src/components/Attributions.svelte
+++ src/components/Attributions.svelte
@@ -10,6 +10,7 @@
     <!-- <a href="https://thenounproject.com/icon/pin-1069069/">Pin by Ralf Schmitzer from NounProject.com</a>, -->
     <a href="https://thenounproject.com/icon/pin-1233347/">Pin by Drishya from NounProject.com</a>,
     <a href="https://thenounproject.com/icon/co2-3844972/">Co2 by Soremba from NounProject.com</a>,
+    <a href="https://thenounproject.com/icon/warning-936898/">Warning by Thengakola from NounProject.com</a>
   </p>
 </div>
 
--- src/components/WarningMessage.svelte
+++ src/components/WarningMessage.svelte
@@ -1,3 +1,32 @@
-<p>
-  <slot/>
-</p>
+<script>
+  import WarningIcon from './icons/Warning.svelte';
+</script>
+
+<aside>
+  <WarningIcon classDefs='warning-icon'/>
+  <p>
+    <slot/>
+  </p>
+</aside>
+
+<style>
+  aside {
+    --warning-colour: rgb(208, 55, 0);
+    display: flex;
+    align-items: center;
+    border: 3px solid var(--warning-colour);
+    padding: 1em;
+    gap: 1em;
+    margin-bottom: 1em;
+  }
+  aside :global(svg) {
+    width: 3em;
+    float: left;
+  }
+  aside :global(.warning-icon) {
+    fill: var(--warning-colour);
+  }
+  p {
+    margin: 0;
+  }
+</style>
