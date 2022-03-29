564f6cb3472af3f0b11e5c17e1c987a0f32922b5
Giles Dring <giles@dringtech.com>
2022-03-29 12:18:50 +0100

    Make sane units for weight

--- src/lib/units.js
+++ src/lib/units.js
@@ -1,7 +1,17 @@
 const prefix = ['', 'k', 'M', 'G', 'T'];
 
+const GRAMS = 'g';
+const gramsUnits = ['g', 'kg', ' tons']
+
+const gramPrefixed = (value) => {
+  if (value < 100) return value.toFixed(1) + gramsUnits[0];
+  if (value < 1e5) return (value / 1000).toFixed(1) + gramsUnits[1];
+  return Math.floor(value / 1e6) + gramsUnits[2];
+}
+
 export const prefixed = (value, unit = '') => {
   if (!value) return 0 + unit;
+  if (unit == GRAMS) return gramPrefixed(value);
   const exponent = Math.floor(Math.log10(value) / 3);
   return (value / (10 ** (3 * exponent))) + prefix[exponent] + unit;
 }
