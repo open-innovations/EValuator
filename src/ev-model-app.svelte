852c31636bec306586e1635ed38463910e884b43
Giles Dring <giles@dringtech.com>
2022-03-31 15:25:13 +0100

    Add legend to map
    closes #17

--- src/ev-model-app.svelte
+++ src/ev-model-app.svelte
@@ -3,6 +3,7 @@
   import Marker from './components/leaflet/Marker.svelte';
   import GeoJson from './components/leaflet/GeoJson.svelte'; 
   import Popup from './components/leaflet/Popup.svelte';
+  import Control from './components/leaflet/Control.svelte';
 
   import ModelPane from './components/ModelPane.svelte';
   import Attributions from './components/Attributions.svelte';
@@ -32,6 +33,14 @@
   };
 
   const filter = (feature) => !(feature.geometry.type === 'Point');
+
+  const layerName = (key) => {
+    const n = key.replace(/^\w/, c => c.toUpperCase())
+    const l = style[key]();
+    return `<svg viewBox="-2 -2 14 14" class="key-code" stroke="${l.color}" fill="${ l.color }">
+      <rect width=10 height=10></rect>
+    </svg> ${ n }`;
+  }
 </script>
 
 <h1>Stage 2: Modelling</h1>
@@ -44,13 +53,25 @@
       </Popup>
     </Marker>
     <GeoJson feature={ $location?.msoa } bind:layer={ msoaOutline } style={ style.msoaFocus }></GeoJson>
-    <GeoJson feature={ $location?.parking } style={ style.parking } { filter }></GeoJson>
-    <GeoJson feature={ $location?.supermarket } style={ style.supermarket } { filter }></GeoJson>
-    <GeoJson feature={ $location?.distribution } style={ style.distribution } { filter }></GeoJson>
-    <GeoJson feature={ $location?.warehouse } style={ style.warehouse } { filter }></GeoJson>
+    <Control>
+      {#each ['parking', 'supermarket', 'distribution', 'warehouse'] as layer }
+        {#if $location }
+          <GeoJson feature={ $location[layer] } style={ style[layer] } { filter } name={ layerName(layer) }></GeoJson>
+        {/if}
+      {/each}
+    </Control>
   </Leaflet>
 </div>
 
 <ModelPane></ModelPane>
 
 <Attributions></Attributions>
+
+<style>
+  :global(.key-code) {
+    width: 2em;
+    vertical-align: bottom;
+    fill-opacity: 0.2;
+    stroke-width: 1.5;
+  }
+</style>
